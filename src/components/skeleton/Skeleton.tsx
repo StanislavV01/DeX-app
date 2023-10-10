5a2396deb07830be591f04bea63959c0e1cefe14
StanislavV01 <nixeeer@gmail.com>
2023-10-11 02:17:36 +0300

    add skeleton  component

---
+++ src/components/skeleton/Skeleton.tsx
@@ -0,0 +1,14 @@
+import Skeleton from "react-loading-skeleton";
+import styles from './skeleton.module.css';
+
+const SkeletonLoader = (): JSX.Element => {
+
+	return (
+		<>
+			Loading...
+		</>
+	)
+}
+
+
+export default SkeletonLoader; 
