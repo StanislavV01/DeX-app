35328e6bf7db4d77c6c47ef231ad224296da34d2
StanislavV01 <nixeeer@gmail.com>
2023-10-11 13:04:36 +0300

    add main.tsx and App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
+import { ThemeProvider } from './Context/ThemeContext.tsx'
+import { MetaMaskContextProvider } from './Context/MetaMaskContext.tsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ThemeProvider>
+        <MetaMaskContextProvider>
+          <App />
+        </MetaMaskContextProvider>
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
